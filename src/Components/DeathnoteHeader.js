7bf96b53170a42d8e1cf227e9580a4ef423d1234
DANNIEL <donghoon149@gmail.com>
2021-06-19 12:46:31 +0900

    Refactor: Component Setting

---
+++ src/Components/DeathnoteHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Styles/component/deathnoteHeader.scss';
+const DeathnoteHeader = () => {
+    /*
+비즈니스 로직
+*/
+
+    return <div className="test">DeathnoteHeader</div>;
+};
+
+export default DeathnoteHeader;
