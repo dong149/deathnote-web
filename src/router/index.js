990076caf521493f3bfedda18998966a48b1fd12
DANNIEL <donghoon149@gmail.com>
2021-06-21 22:44:03 +0900

    Add: sitemap.xml&ga tag

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import {
-    BrowserRouter,
-    Route,
-    withRouter,
-    Switch,
-    Router,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './main';
 import '../Styles/styles.scss';
 import Summoner from './summoner';
 import DeathnoteFunction from './deathnoteFunction';
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
+export const EntryRoute = () => {
+    ReactGA.initialize(process.env.REACT_APP_TRACKING_ID, { debug: true });
+    const history = createBrowserHistory();
+    history.listen((location) => {
+        ReactGA.set({ set: location.pathname }); // Update the user's current page
+        ReactGA.pageview(location.pathname); // Record a pageview for the given page
+    });
 
-export const EntryRoute = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Main} />
-            <Route path="/summoner/:name" component={Summoner} />
-            <Route path="/deathnote/function" component={DeathnoteFunction} />
-        </Switch>
-    </BrowserRouter>
-);
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/summoner/:name" component={Summoner} />
+                <Route
+                    path="/deathnote/function"
+                    component={DeathnoteFunction}
+                />
+            </Switch>
+        </BrowserRouter>
+    );
+};
